0faa82f26561d7d7f6ffd7d5c6a9cede4808a0af
Lyzi Diamond <lyzidiamond@gmail.com>
2015-02-26 12:22:19 -0800

    tighten description language

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var jsts = require('jsts');
 
 /**
- * Finds the difference between two polygons by clipping the second
+ * Finds the difference between two {@link Polygon|polygons} by clipping the second
  * polygon from the first.
  *
  * @module turf/erase
