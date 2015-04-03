0becffe469da3021b58fe61d42ff43238165c7c2
Tim Channell <tim.channell@gmail.com>
2015-04-03 07:59:48 -0400

    renamed erase to difference

--- bench.js
+++ bench.js
@@ -1,17 +1,17 @@
-var erase = require('./');
+var difference = require('./');
 var Benchmark = require('benchmark');
 var fs = require('fs');
 
 var clip = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/erasedHole.geojson'));
 var hole = JSON.parse(fs.readFileSync(__dirname+'/test/fixtures/in/erasedFC.geojson'));
 
-var suite = new Benchmark.Suite('turf-erase');
+var suite = new Benchmark.Suite('turf-difference');
 suite
-  .add('turf-erase#clip',function () {
-    erase(clip[0], clip[1]);
+  .add('turf-difference#clip',function () {
+    difference(clip[0], clip[1]);
   })
-  .add('turf-erase#hole',function () {
-    erase(hole[0], hole[1]);
+  .add('turf-difference#hole',function () {
+    difference(hole[0], hole[1]);
   })
   .on('cycle', function (event) {
     console.log(String(event.target));
